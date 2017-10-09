73f467290cbd43ece926610ecfd27714d1e21850
Karan Atwal <karan.atwal@wns.com>
2017-10-09 18:03:22 -0400

    Adding get route

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const {mongoose} = require('./db/mongoose.js');
 const {Todo} = require('./models/todo.js');
 const {User} = require('./models/user.js');
 
-var app =express();
+var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
@@ -16,12 +16,20 @@ app.post('/todos', (req, res) => {
   todo.save().then((doc) => {
     res.send(doc);
   }, (err) => {
-    res.status(400).send(err)
+    res.status(400).send(err);
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
+
 app.listen(3000, () => {
-  console.log('server is up on port 3000 ')
+  console.log('server is up on port 3000 ');
 });
 
 module.exports = {app};
