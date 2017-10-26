2ca49fcc0a365b9eb7a6591e227035336259389d
Karan Atwal <karan.atwal@wns.com>
2017-10-26 18:24:17 -0400

    Add test patch

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,8 @@ const todos = [{
   text: 'First todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second todo'
+  text: 'Second todo',
+  completed: true
 }]
 
 beforeEach((done) => {
@@ -138,5 +139,41 @@ describe('DELETE /todos/:id', () => {
       .delete('/todos/123abc')
       .expect(404)
       .end(done);
+  });
+});
+
+describe('PATCH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var id = todos[0]._id.toHexString();
+    var text = "Updated text";
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+      })
+      .end(done)
+  });
+
+  it('should clear completedAt when completed is set to false', (done) => {
+    var id = todos[1]._id.toHexString();
+
+    request(app)
+      .patch(`/todos/${id}`)
+      .send({
+        completed: false
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBe(null);
+      })
+      .end(done)
   });
 });
